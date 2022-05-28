9c72485ccf23b6107cdcf0cb4ed1a5dd67e9c5a2
reiya ono <reiya.ono@linkbal.co.jp>
2022-05-28 14:35:16 +0900

    develop js

--- src/content.js
+++ src/content.js
@@ -1,13 +1,24 @@
-window.alert('アプリ開いたね！');
+const POLLING_MS = 2000;
 
-const POLLING_MS = 5000;
+const createNotifier = (fileNum) => {
+  const options = {
+    body: `${fileNum}件のアップロードが完了しました`,
+    icon: 'https://gigafile.nu/favicon.ico'
+  }
+  const title = 'UploadNotifier'
+  new Notification(title, options);
+}
 
 const checkStatus = () => {
-  const isComplete = Object.values(document.querySelectorAll("span.status")).reduce((result,v) => {
+  const statusDoms = document.querySelectorAll("span.status")
+  if (statusDoms.length === 0) return
+  const isComplete = Object.values(statusDoms).reduce((result,v) => {
     result.push(v.innerText)
-    return result 
+    return result
    }, []).every(v => v === '完了！');
-   console.log(isComplete)
+  if (!isComplete) return
+  createNotifier(statusDoms.length)
+  clearInterval(timer)
 };
 
-setInterval(checkStatus, POLLING_MS);
+const timer = setInterval(checkStatus, POLLING_MS);
